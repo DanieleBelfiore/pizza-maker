2b01cbfe582c87ebdc5c008487dfc229cb0155dc
Daniele Belfiore <d.belfiore@agricolus.com>
2024-07-05 16:51:20 +0200

    Update recipe

--- src/views/HomeView.ts
+++ src/views/HomeView.ts
@@ -1,15 +1,16 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  data() {
+  data()
+  {
     return {
       quantity: null as number | null,
       recipe: false as boolean,
       recipeSuggestion: true as boolean,
-      water: 121 as number,
-      flour: 183 as number,
-      yeast: 0.6 as number,
-      salt: 4 as number,
+      water: 116 as number,
+      flour: 166 as number,
+      yeast: 0.5 as number,
+      salt: 3 as number,
     }
   },
   methods: {},
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,8 @@
   <div class="title">
     <h1>Vuoi creare l'arte della pizza?!</h1>
     <img class="logo" alt="logo" src="@/assets/logo.png" />
-    <h2>Quante pizze da ~280gr vuoi fare? <input class="quantity" type="number" min="1" max="100" v-model="quantity"></h2>
+    <h2>Quante pizze da ~280gr vuoi fare? <input class="quantity" type="number" min="1" max="100" v-model="quantity">
+    </h2>
   </div>
   <Transition>
     <div class="ingredients-container" v-if="quantity">
@@ -25,23 +26,24 @@
       </div>
       <Transition>
         <div class="recipe" v-if="recipe">
-          <strong>Procedimento (~24-28h prima di mangiarla)</strong>:<br>
+          <strong>Procedimento (almeno ~24h prima di mangiarla)</strong>:<br>
           <ul>
             <li>Mettere nel boccale dell'impastatrice la farina e il lievito.</li>
             <li>Preparare l'acqua in una caraffa.</li>
             <li>Attivare la modalità impastatrice a 0° a velocità 4 per 4min.</li>
-            <li>Mentre sta impastando, aggiungere a filo l'acqua e, a meno di metà acqua, il sale e l'acqua restante.</li>
-            <li>Lasciare riposare ~30min nel boccale.</li>
+            <li>Mentre sta impastando, aggiungere a filo l'acqua e, a meno di metà acqua, il sale e l'acqua restante.
+            </li>
+            <li>Lasciare riposare nel boccale chiuso in frigo per ~30min.</li>
             <li>Piegare il panetto con le classiche pieghe a portafoglio.</li>
-            <li>Mettere l'impasto in un contenitore con coperchio e lasciar lievitare a temperatura ambiente per ~2h.</li>
-            <li>Mettere il contenitore in frigo per ~24h.</li>
-            <li>~3-4h prima di infornare, togliere l'impasto dal frigo e dopo ~30min dividere in panetti da ~280gr.</li>
-            <li>Far lievitare per ~3h.</li>
+            <li>Mettere l'impasto in un contenitore con coperchio e lasciar lievitare a temperatura ambiente per ~1h.
+            </li>
+            <li>Ripiegare nuovamente il panetto con le classiche pieghe a portafoglio.
+            </li>
+            <li>Aggiungere un filo d'olio nel contenitore e mettere l'impasto in frigo per almeno ~24h.</li>
+            <li>~4h prima di infornare, togliere l'impasto dal frigo e fare lo staglio dividendo in panetti da ~270gr.
+            </li>
             <li>Stendere e condire a piacimento.</li>
-            <li>Al momento della cottura, inserire da subito la pietra refrattaria (o la teglia) in forno.</li>
-            <li>Accendere il forno in modalità ventilato alla massima temperatura per ~30min.</li>
           </ul>
-          <u>Nota: per doppia dose fare 2 panetti divisi.</u><br>
         </div>
       </Transition>
       <Transition>
