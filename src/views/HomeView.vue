a0e5c46a439ac7f2e9a20fb614e6a9d9530ec9c1
Daniele Belfiore <danielebelfiorepc@gmail.com>
2026-02-27 12:53:18 +0100

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,10 +57,8 @@
     <div class="ingredients-container" v-if="quantity">  
       <div class="content-grid">
         <div class="ingredients">
-          <div>
-            <img class="pizza" alt="pizza" src="@/assets/pizza.png" />
-          </div>
           <div class="list">
+            <img class="pizza" alt="ingredients" src="@/assets/ingredients.png" />
             <h3>Ingredienti:</h3>
             <ul>
               <li>Farina 0 (Caputo Nuvola): {{ Math.round(quantity * flour / 5) * 5 }} gr</li>
@@ -71,25 +69,30 @@
           </div>
         </div>
         <div class="recipe">
-          <h3>Procedimento (almeno ~24 ore prima di mangiarla):</h3>
-          <ul>
-            <li>Inserire nell'impastatrice il <strong>gancio di tipo foglia</strong>, amalgamerà meglio l'impasto.</li>
-            <li>Mettere nel boccale <strong>farina</strong> e <strong>lievito</strong> e iniziare a impastare lentamente (<strong>velocità 1</strong>) per farli mischiare.</li>
-            <li>Mentre sta impastando, aggiungere lentamente il <strong>~60% dell'acqua</strong> per creare un impasto compatto aumentando man mano la velocità dell'impastatrice (<strong>velocità 2-3</strong>).</li>
-            <li>Quando la vasca risulta pulita e l'impasto si <strong>attorciglia alla foglia</strong>, si aggiunge la <strong>restante acqua</strong> poco alla volta, il <strong>sale</strong> e l'ultima goccia d'acqua per favorirne l'assorbimento.</li>
-            <li>Dopo che si è amalgamato bene tutto l'impasto, fermare tutto e mettere il boccale coperto in <strong>frigo</strong> per <strong>~10 minuti</strong>, questo aiuterà la <strong>maglia glutinica</strong> e a non superare i <strong>~23°</strong> durante la lavorazione.</li>
-            <li>Si riprende a impastare a velocità sostenuta (<strong>velocità 4</strong>) per dare forza e <strong>incordare l'impasto</strong> finché non si stacca dalle pareti e rimane attaccato alla foglia.</li>
-            <li>Stendere l'impasto sul banco e attendere <strong>~5 minuti</strong> per farlo <strong>riposare</strong>.</li>
-            <li>Chiudere il panetto con le classiche <strong>pieghe a portafoglio</strong> e mettere l'impasto in un contenitore aggiungendo un filo d'<strong>olio</strong> nel fondo e lasciarlo <strong>lievitare</strong> con coperchio a temperatura ambiente per <strong>~1 ora</strong>.</li>
-            <li>Fare la <strong>piega di rinforzo</strong> chiudendo nuovamente il panetto con le classiche <strong>pieghe a portafoglio</strong>.</li>
-            <li>Aggiungere se necessario nuovamente un filo d'<strong>olio</strong> nel fondo del contenitore e mettere l'impasto con il coperchio in <strong>frigo</strong> per almeno <strong>~24 ore</strong>.</li>
-            <li><strong>~3-4 ore prima di infornare</strong>, togliere l'impasto dal frigo e fare lo <strong>staglio</strong> dividendo in panetti da <strong>~280 grammi</strong>.</li>
-            <li>Far <strong>lievitare</strong> i panetti a temperatura ambiente in un contenitore da pizza chiuso.</li>
-            <li><strong>Stendere</strong> e <strong>condire</strong> a piacimento!</li>
-          </ul>
-          <h2>Buon appetito! 😋</h2>
+          <div class="list">
+            <img class="pizza" alt="recipe" src="@/assets/recipe.png" />
+            <h3>Procedimento (~24 ore prima):</h3>
+            <ul>
+              <li>Inserire nell'impastatrice il <strong>gancio di tipo foglia</strong>, amalgamerà meglio l'impasto.</li>
+              <li>Mettere nel boccale <strong>farina</strong> e <strong>lievito</strong> e iniziare a impastare lentamente (<strong>velocità 1</strong>) per farli mischiare.</li>
+              <li>Mentre sta impastando, aggiungere lentamente il <strong>~60% dell'acqua</strong> per creare un impasto compatto aumentando man mano la velocità dell'impastatrice (<strong>velocità 2-3</strong>).</li>
+              <li>Quando la vasca risulta pulita e l'impasto si <strong>attorciglia alla foglia</strong>, si aggiunge la <strong>restante acqua</strong> poco alla volta, il <strong>sale</strong> e l'ultima goccia d'acqua per favorirne l'assorbimento.</li>
+              <li>Dopo che si è amalgamato bene tutto l'impasto, fermare tutto e mettere il boccale coperto in <strong>frigo</strong> per <strong>~10 minuti</strong>, questo aiuterà la <strong>maglia glutinica</strong> e a non superare i <strong>~23°</strong> durante la lavorazione.</li>
+              <li>Si riprende a impastare a velocità sostenuta (<strong>velocità 4</strong>) per dare forza e <strong>incordare l'impasto</strong> finché non si stacca dalle pareti e rimane attaccato alla foglia.</li>
+              <li>Stendere l'impasto sul banco e attendere <strong>~5 minuti</strong> per farlo <strong>riposare</strong>.</li>
+              <li>Chiudere il panetto con le classiche <strong>pieghe a portafoglio</strong> e mettere l'impasto in un contenitore aggiungendo un filo d'<strong>olio</strong> nel fondo e lasciarlo <strong>lievitare</strong> con coperchio a temperatura ambiente per <strong>~1 ora</strong>.</li>
+              <li>Fare la <strong>piega di rinforzo</strong> chiudendo nuovamente il panetto con le classiche <strong>pieghe a portafoglio</strong>.</li>
+              <li>Aggiungere se necessario nuovamente un filo d'<strong>olio</strong> nel fondo del contenitore e mettere l'impasto con il coperchio in <strong>frigo</strong> per almeno <strong>~24 ore</strong>.</li>
+              <li><strong>~3-4 ore prima di infornare</strong>, togliere l'impasto dal frigo e fare lo <strong>staglio</strong> dividendo in panetti da <strong>~280 grammi</strong>.</li>
+              <li>Far <strong>lievitare</strong> i panetti a temperatura ambiente in un contenitore da pizza chiuso.</li>
+              <li><strong>Stendere</strong> e <strong>condire</strong> a piacimento!</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </Transition>
+  <div class="footer" v-if="quantity">
+    <h2>Buon Appetito! 😋</h2>
+  </div>
 </template>
